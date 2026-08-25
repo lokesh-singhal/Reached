09f0c85f04431ee0072d14cc40e319977578536a
lokesh-singhalgit config --global user.name lokesh-singhalgit config --global user.name lokesh-singhal <lokeshsinghal900@gmail.com>
2026-08-25 18:43:40 +0530

    feat: update authClient to use environment variable for baseURL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,5 +5,5 @@ export const authClient = createAuthClient({
         usernameClient() 
     ],
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.BETTER_AUTH_URL
 })
